51d300280d9effe7619189b2f8778d1f94151b54
Waptech <abdulfataiyakub07@gmail.com>
2025-03-06 12:50:17 +0100

    feat: add color constant in utils

---
+++ lib/utils/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryGreen = Color(0xFF3AB03A);
+  static const Color secondaryRed = Color(0xFFDC4848);
+  static const Color background = Color(0xFFFBFBFB);
+  static const Color textDark = Color(0xFF3E3E3E);
+  static const Color starYellow = Color(0xFFFFC107);
+}
