79f14196584fffeb700e6cd70161dc6a8b35bd47
Waptech <abdulfataiyakub07@gmail.com>
2025-03-07 13:46:50 +0100

    feat: add three responsive layouts i.e mobile ,tablet, desktop. for service section

--- lib/widgets/service_section.dart
+++ lib/widgets/service_section.dart
@@ -72,6 +72,27 @@ class ServiceSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        // Three breakpoints:
+        bool isDesktop = constraints.maxWidth >= 1200;
+        bool isTablet =
+            constraints.maxWidth < 1200 && constraints.maxWidth >= 600;
+        bool isMobile = constraints.maxWidth < 600;
+
+        if (isDesktop) {
+          return _buildDesktopLayout(context);
+        } else if (isTablet) {
+          return _buildTabletLayout(context);
+        } else {
+          return _buildMobileLayout(context);
+        }
+      },
+    );
+  }
+
+  // Desktop Layout
+  Widget _buildDesktopLayout(BuildContext context) {
     return Container(
       color: AppColors.background,
       padding: EdgeInsets.only(
@@ -80,50 +101,178 @@ class ServiceSection extends StatelessWidget {
         right: 156.28.w,
         bottom: 40.89.h,
       ),
-      child: LayoutBuilder(
-        builder: (context, constraints) {
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "Services Categories",
-                style: GoogleFonts.poppins(
-                  fontSize: 25.sp,
-                  fontWeight: FontWeight.bold,
-                  color: AppColors.secondaryRed,
-                ),
-              ),
-              SizedBox(height: 20.h),
-              GridView.builder(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                itemCount: services.length,
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 4,
-                  mainAxisSpacing: 15.h,
-                  crossAxisSpacing: 39.55.w,
-                ),
-                itemBuilder: (context, index) {
-                  final service = services[index];
-                  return InkWell(
-                      onTap: () {
-                        if (service["title"] == "Construction and Building") {
-                          _showConstructionServices(context);
-                        } else {
-                          // nothing yet
-                        }
-                      },
-                      child: _buildServiceCard(service));
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Services Categories",
+            style: GoogleFonts.poppins(
+              fontSize: 25.sp,
+              fontWeight: FontWeight.bold,
+              color: AppColors.secondaryRed,
+            ),
+          ),
+          SizedBox(height: 20.h),
+          GridView.builder(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: services.length,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 4,
+              mainAxisSpacing: 20.h,
+              crossAxisSpacing: 19.55.w,
+            ),
+            itemBuilder: (context, index) {
+              final service = services[index];
+              return InkWell(
+                onTap: () {
+                  if (service["title"] == "Construction and Building") {
+                    _showConstructionServices(context);
+                  }
                 },
-              ),
-            ],
-          );
-        },
+                child: _buildServiceCard(service, layout: 'desktop'),
+              );
+            },
+          ),
+        ],
       ),
     );
   }
 
-  Widget _buildServiceCard(Map<String, String> service) {
+  // Tablet Layout
+  Widget _buildTabletLayout(BuildContext context) {
+    return Container(
+      color: AppColors.background,
+      padding: EdgeInsets.symmetric(horizontal: 40.w, vertical: 40.h),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Services Categories",
+            style: GoogleFonts.poppins(
+              fontSize: 45.sp,
+              fontWeight: FontWeight.bold,
+              color: AppColors.secondaryRed,
+            ),
+          ),
+          SizedBox(height: 20.h),
+          GridView.builder(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: services.length,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              mainAxisSpacing: 20.h,
+              crossAxisSpacing: 30.w,
+            ),
+            itemBuilder: (context, index) {
+              final service = services[index];
+              return InkWell(
+                onTap: () {
+                  if (service["title"] == "Construction and Building") {
+                    _showConstructionServices(context);
+                  }
+                },
+                child: _buildServiceCard(service, layout: 'tablet'),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  //Mobile Layout
+  Widget _buildMobileLayout(BuildContext context) {
+    return Container(
+      color: AppColors.background,
+      padding: EdgeInsets.all(20.w),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Services Categories",
+            style: GoogleFonts.poppins(
+              fontSize: 55.sp,
+              fontWeight: FontWeight.bold,
+              color: AppColors.secondaryRed,
+            ),
+          ),
+          SizedBox(height: 20.h),
+          GridView.builder(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: services.length,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              mainAxisSpacing: 20.h,
+              crossAxisSpacing: 50.w,
+            ),
+            itemBuilder: (context, index) {
+              final service = services[index];
+              return InkWell(
+                onTap: () {
+                  if (service["title"] == "Construction and Building") {
+                    _showConstructionServices(context);
+                  }
+                },
+                child: _buildServiceCard(service, layout: 'mobile'),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  // Reusable Service Card
+
+  Widget _buildServiceCard(Map<String, String> service,
+      {required String layout}) {
+    double cardHeight;
+    double cardWidth;
+    double ratingCircleRadius;
+    double ratingFontSize;
+    double titleFontSize;
+    double borderRadius;
+    double imageFitHeight;
+    double imageFitWidth;
+
+    // Adjust styling based on layout
+    switch (layout) {
+      case 'desktop':
+        cardHeight = 177.45;
+        cardWidth = 243.69.w;
+        ratingCircleRadius = 38.r;
+        ratingFontSize = 18.sp;
+        titleFontSize = 18.sp;
+        borderRadius = 20.r;
+        imageFitHeight = 177.45;
+        imageFitWidth = 243.69.w;
+        break;
+
+      case 'tablet':
+        cardHeight = 150;
+        cardWidth = double.infinity;
+        ratingCircleRadius = 30.r;
+        ratingFontSize = 20.sp;
+        titleFontSize = 20.sp;
+        borderRadius = 15.r;
+        imageFitHeight = 200.h;
+        imageFitWidth = double.infinity;
+        break;
+
+      default: // mobile
+        cardHeight = 150;
+        cardWidth = double.infinity;
+        ratingCircleRadius = 50.r;
+        ratingFontSize = 30.sp;
+        titleFontSize = 40.sp;
+        borderRadius = 30.r;
+        imageFitHeight = 150;
+        imageFitWidth = double.infinity;
+    }
+
     return Stack(
       clipBehavior: Clip.none,
       children: [
@@ -131,28 +280,30 @@ class ServiceSection extends StatelessWidget {
           mainAxisSize: MainAxisSize.min,
           children: [
             Container(
-              height: 177.45,
-              width: 243.69.w,
+              height: cardHeight,
+              width: cardWidth,
               decoration: BoxDecoration(
                 color: Colors.white,
                 boxShadow: [
                   BoxShadow(
-                    color: Color(0x40000000),
-                    offset: Offset(-4, 5),
+                    color: const Color(0x40000000),
+                    offset: const Offset(-4, 5),
                     blurRadius: 8,
                   ),
                 ],
-                borderRadius: BorderRadius.circular(20.r),
+                borderRadius: BorderRadius.circular(borderRadius),
               ),
               child: ClipRRect(
-                borderRadius: BorderRadius.circular(20.r),
+                borderRadius: BorderRadius.circular(borderRadius),
                 child: Image.asset(
                   service["image"]!,
                   fit: BoxFit.contain,
+                  height: imageFitHeight,
+                  width: imageFitWidth,
                 ),
               ),
             ),
-            SizedBox(height: 18.93.h),
+            SizedBox(height: layout == 'desktop' ? 18.93.h : 10.h),
             Text(
               service["title"]!,
               textAlign: TextAlign.center,
@@ -160,23 +311,24 @@ class ServiceSection extends StatelessWidget {
               overflow: TextOverflow.ellipsis,
               style: GoogleFonts.monda(
                 fontWeight: FontWeight.bold,
-                fontSize: 18.sp,
+                fontSize: titleFontSize,
                 color: Colors.black,
               ),
             ),
           ],
         ),
+        // Rating Circle
         Positioned(
-          top: -10.h,
-          right: -10.w,
+          top: layout == 'desktop' ? -10.h : -20.h,
+          right: layout == 'desktop' ? -10.w : -20.w,
           child: CircleAvatar(
-            radius: 38.r,
+            radius: ratingCircleRadius,
             backgroundColor: AppColors.primaryGreen,
             child: Text(
               service["rating"]!,
               style: TextStyle(
                 color: Colors.white,
-                fontSize: 18.sp,
+                fontSize: ratingFontSize,
                 fontWeight: FontWeight.w700,
               ),
             ),
@@ -186,7 +338,7 @@ class ServiceSection extends StatelessWidget {
     );
   }
 
-  /// Shows the construction jobs in a popup dialog
+  // Show popup dialog for Construction & Building
   void _showConstructionServices(BuildContext context) {
     showDialog(
       context: context,
