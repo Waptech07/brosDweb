c23c8cd5440c348ea261f7e35bd5dced581bb7e8
Waptech <abdulfataiyakub07@gmail.com>
2025-03-07 10:57:17 +0100

    feat: add responsive mobile layout for top navbar

--- lib/widgets/top_navbar.dart
+++ lib/widgets/top_navbar.dart
@@ -8,84 +8,182 @@ class TopNavbar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(
-        left: 79.w,
-        top: 29.h,
-        bottom: 11.h,
-        // right: 43.w,
-      ),
-      width: double.infinity,
-      color: Colors.white,
-      child: Row(
-        // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Container(
-            width: 677.w,
-            height: 55.h,
-            child: TextField(
-              decoration: InputDecoration(
-                suffixIcon: Image.asset(
-                  'assets/images/search-icon.png',
-                  height: 21.88.h,
-                  width: 25.24.w,
-                ),
-                enabled: true,
-                contentPadding:
-                    EdgeInsets.symmetric(vertical: 10.h, horizontal: 16.w),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12.62.r),
-                  borderSide: BorderSide(
-                    width: 0.84.w,
-                    color: const Color(0xFFC8C4C4),
-                  ),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12.62.r),
-                  borderSide: BorderSide(
-                    width: 0.84.w,
-                    color: AppColors.primaryGreen,
-                  ),
-                ),
-                filled: true,
-                fillColor: const Color(0xFFF1F1F1),
+    return LayoutBuilder(builder: (context, constraints) {
+      bool isMobile = constraints.maxWidth < 800;
+
+      return Container(
+        padding: EdgeInsets.only(
+          left: isMobile ? 20.w : 79.w,
+          top: isMobile ? 20.h : 29.h,
+          bottom: isMobile ? 20.h : 11.h,
+          right: isMobile ? 20.w : 0,
+        ),
+        width: double.infinity,
+        color: Colors.white,
+        child: isMobile
+            ? _buildMobileLayout(context)
+            : _buildDesktopLayout(context),
+      );
+    });
+  }
+
+  /// Desktop layout
+  Widget _buildDesktopLayout(BuildContext context) {
+    return Row(
+      // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Container(
+          width: 677.w,
+          height: 55.h,
+          child: TextField(
+            decoration: InputDecoration(
+              suffixIcon: Image.asset(
+                'assets/images/search-icon.png',
+                height: 21.88.h,
+                width: 25.24.w,
               ),
+              enabled: true,
+              contentPadding:
+                  EdgeInsets.symmetric(vertical: 10.h, horizontal: 16.w),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(12.62.r),
+                borderSide: BorderSide(
+                  width: 0.84.w,
+                  color: const Color(0xFFC8C4C4),
+                ),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(12.62.r),
+                borderSide: BorderSide(
+                  width: 0.84.w,
+                  color: AppColors.primaryGreen,
+                ),
+              ),
+              filled: true,
+              fillColor: const Color(0xFFF1F1F1),
             ),
           ),
-          SizedBox(width: 233.w),
-          Container(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Welcome, Amanda',
-                  style: GoogleFonts.poppins(
-                    fontSize: 24.sp,
-                    fontWeight: FontWeight.w500,
-                    color: AppColors.primaryGreen,
-                  ),
-                ),
-                Text(
-                  'Tue, 07 November 2024',
-                  style: GoogleFonts.poppins(
-                    fontSize: 16.sp,
-                    fontWeight: FontWeight.w300,
-                    color: Colors.black,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          SizedBox(width: 51.w),
-          Row(
+        ),
+        SizedBox(width: 233.w),
+        Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Image.asset('assets/images/notification-icon.png'),
-              SizedBox(width: 34.w),
-              Image.asset('assets/images/profile.png'),
+              Text(
+                'Welcome, Amanda',
+                style: GoogleFonts.poppins(
+                  fontSize: 24.sp,
+                  fontWeight: FontWeight.w500,
+                  color: AppColors.primaryGreen,
+                ),
+              ),
+              Text(
+                'Tue, 07 November 2024',
+                style: GoogleFonts.poppins(
+                  fontSize: 16.sp,
+                  fontWeight: FontWeight.w300,
+                  color: Colors.black,
+                ),
+              ),
             ],
           ),
-        ],
-      ),
+        ),
+        SizedBox(width: 51.w),
+        Row(
+          children: [
+            Image.asset('assets/images/notification-icon.png'),
+            SizedBox(width: 34.w),
+            Image.asset('assets/images/profile.png'),
+          ],
+        ),
+      ],
+    );
+  }
+
+  // Mobile layout
+  Widget _buildMobileLayout(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          width: double.infinity,
+          height: 55.h,
+          padding: EdgeInsets.symmetric(
+            horizontal: 40.w,
+          ),
+          child: TextField(
+            decoration: InputDecoration(
+              suffixIcon: Image.asset(
+                'assets/images/search-icon.png',
+              ),
+              enabled: true,
+              contentPadding:
+                  EdgeInsets.symmetric(vertical: 10.h, horizontal: 16.w),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(12.62.r),
+                borderSide: BorderSide(
+                  width: 0.84.w,
+                  color: const Color(0xFFC8C4C4),
+                ),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(12.62.r),
+                borderSide: BorderSide(
+                  width: 0.84.w,
+                  color: AppColors.primaryGreen,
+                ),
+              ),
+              filled: true,
+              fillColor: const Color(0xFFF1F1F1),
+            ),
+          ),
+        ),
+        SizedBox(height: 20.h),
+        Container(
+          padding: EdgeInsets.symmetric(
+            horizontal: 30.w,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Welcome, Amanda',
+                      style: GoogleFonts.poppins(
+                        fontSize: 50.sp,
+                        fontWeight: FontWeight.w500,
+                        color: AppColors.primaryGreen,
+                      ),
+                    ),
+                    Text(
+                      'Tue, 07 November 2024',
+                      style: GoogleFonts.poppins(
+                        fontSize: 44.sp,
+                        fontWeight: FontWeight.w300,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Row(
+                children: [
+                  Image.asset(
+                    'assets/images/notification-icon.png',
+                  ),
+                  SizedBox(width: 50.w),
+                  Image.asset(
+                    'assets/images/profile.png',
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
